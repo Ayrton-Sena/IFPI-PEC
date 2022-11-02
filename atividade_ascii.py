90a60fe62f911ed78c304d05e1159b8a26b01e47
Ayrton-Sena <104458817+Ayrton-Sena@users.noreply.github.com>
2022-11-02 15:43:58 -0300

    Criei o arquivo atividade_ascii.py

---
+++ atividade_ascii.py
@@ -0,0 +1,62 @@
+print("Uma imagem de um cachorro...")
+print(" 0____ ")
+print("  |||| ")
+print()
+print('''
+Uma imagem de um cachorro...
+ 0____
+  ||||
+''')
+print()
+print('''
+Meu animal favorito é o porco:
+
+    ---     ---
+    \ / --- \ /
+      ).  .(
+     ( ('') )
+      )    (
+     /      \
+    (        )
+   ( \ /- \ / )
+    w'W    W'w
+
+    
+''')
+print()
+print()
+print("ola " * 5)
+print()
+print("ola, " + "tudo bem?")
+print()
+print("ba" + "na"*2 + "da")
+print("piri"*2)
+print("cobra " + "su"+"ru"+"cu"*2)
+print()
+print("#" * 30)
+print("#" * 30)
+print("#" * 30)
+print()
+print("/\  "*10)
+print("  \/"*10)
+print()
+print("Parabéns!!!!!!!!!! ")
+print()
+print("Aqui vai um dinossauro!")
+print()
+print('''
+''')
+print()
+print("E aqui vai um bolo! hummmmmmmmm")
+print()
+print(" ."*10)
+print(" i"*10)
+print("#"*21)
+print("="*21)
+print("#"*21)
+print("="*21)
+print("#"*21)
+
+
+
+      
